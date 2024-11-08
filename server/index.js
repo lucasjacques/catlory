244b1e698727fe0dbef861ff885b1f843121f2ec
Lucas Jacques <lucas.jacs@gmail.com>
2024-11-08 00:41:57 -0300

    Remove db password from server files

--- server/index.js
+++ server/index.js
@@ -5,10 +5,11 @@ const mysql = require('mysql');
 
 // MySQL connection
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // Replace with your MySQL username
-  password: '1337DONTSTEALMYPASSWORD', // Replace with your MySQL password
-  database: 'calorie_tracker'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
 
 // Connect to MySQL
