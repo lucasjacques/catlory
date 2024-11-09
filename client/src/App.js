9714284fe5ec9b3d5445d46e3fdeed8de30eb0fd
Lucas Jacques <lucas.jacs@gmail.com>
2024-11-08 23:19:23 -0300

    Create initial front

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CalorieForm from './components/CalorieForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Daily Calorie Tracker</h1>
+      <CalorieForm />
+    </div>
+  );
+}
+
+export default App;
